54313aa06ebeed4324f175b5c46ce99bce0aea2a
anujyog1 <agyogita@gmail.com>
2022-11-10 15:59:08 +0530

    fist

---
+++ src/main/java/SEGroup/YMavenJavaProject/App.java
@@ -0,0 +1,13 @@
+package SEGroup.YMavenJavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello world from yogita 10th Nov " );
+    }
+}
