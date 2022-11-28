13cfbd933391d72826e77f90d49d7aedcd061907
anujyog1 <agyogita@gmail.com>
2022-11-28 22:48:03 +0530

    first commit 28th Nov

--- src/main/java/SEGroup/YMavenJavaProject/App.java
+++ src/main/java/SEGroup/YMavenJavaProject/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello world from yogita 10th Nov " );
+        System.out.println( "Hello world from yogita 28th Nov " );
     }
 }
